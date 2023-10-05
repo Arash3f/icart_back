c8b9358a96a039429c0c5f94b7d34da4c7539c36
arash3f <arash.alfooneh@gmail.com>
2023-10-05 11:44:30 +0330

    fix: mechant shecma

--- src/merchant/schema.py
+++ src/merchant/schema.py
@@ -3,6 +3,10 @@ from uuid import UUID
 
 from pydantic import BaseModel, ConfigDict
 
+from src.contract.schema import ContractBase
+from src.location.schema import LocationRead
+from src.user.schema import UserRead
+
 
 # ---------------------------------------------------------------------------
 class MerchantBase(BaseModel):
@@ -16,3 +20,8 @@ class MerchantRead(MerchantBase):
 
     created_at: datetime
     updated_at: datetime | None
+
+    # # ! Relation
+    contract: ContractBase | None = None
+    user: UserRead
+    location: LocationRead | None = None
