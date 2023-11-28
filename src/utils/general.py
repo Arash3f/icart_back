8f65039da91c1503e98a9d97d067192ad4f511d5
arash3f <arash.alfooneh@gmail.com>
2023-11-28 10:37:35 +0330

    fix: quick fix

--- src/utils/general.py
+++ src/utils/general.py
@@ -1,6 +1,4 @@
 def add_arabic_word(text: str) -> str:
     text = text.replace("ی", "ي")
     text = text.replace("ک", "ك")
-    text = text.replace("ه", "ة")
-    text = text.replace("و", "ؤ")
     return text
